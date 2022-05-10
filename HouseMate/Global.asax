bcc02933cc06827c843d349728e7c9aeb9c67714
yasmeenAdel154 <68759700+yasmeenAdel154@users.noreply.github.com>
2022-05-10 22:01:19 +0200

    add new data base creation

---
+++ HouseMate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HouseMate.MvcApplication" Language="C#" %>
---
+++ dbo.reacts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[reacts] (
+    [postID]      INT          NOT NULL,
+    [userID]      INT          NOT NULL,
+    [typeOfReact] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_reacts] PRIMARY KEY CLUSTERED ([postID] ASC, [userID] ASC),
+    FOREIGN KEY ([postID]) REFERENCES [dbo].[posts] ([postID]),
+    FOREIGN KEY ([userID]) REFERENCES [dbo].[users] ([userID])
+);
+
