bb3dc06acf70bf2b833fd78c31861ea6dfbd3c03
yasmeenAdel154 <68759700+yasmeenAdel154@users.noreply.github.com>
2022-05-11 06:30:43 +0200

    add profile and write post

--- HouseMata/Controllers/HomeController.cs
+++ HouseMata/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HouseMata.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,11 @@ namespace HouseMata.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext db = new DataContext();
+         
         public ActionResult Index()
         {
+            
             return View();
         }
 
